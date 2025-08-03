e49730b4b56b25163686ac4a7fcfc5397899dcd5
erodrigufer <erodrigufer@gmail.com>
2025-08-03 03:03:20 +0200

    fix: Footer point to blog not GitHub

--- backend/internal/views/base_templ.go
+++ backend/internal/views/base_templ.go
@@ -42,7 +42,7 @@ func base() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "</div></body><footer>Developed by <a href=\"https://github.com/erodrigufer\" target=\"_blank\">Eduardo Rodriguez </a> &copy; ")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "</div></body><footer>Developed by <a href=\"https://blog.erodriguez.de\" target=\"_blank\">Eduardo Rodriguez </a> &copy; ")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
